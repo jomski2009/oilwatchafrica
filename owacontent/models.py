af98749fd2ed28f506b51e8dd088a8da0b31c205
Jome Akpoduado <jomea@yookos.com>
2016-03-19 21:45:42 +0200

    A bunch of front end customizations

--- owacontent/models.py
+++ owacontent/models.py
@@ -1,12 +1,30 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
+from mezzanine.core.fields import FileField
 from mezzanine.core.models import Displayable, RichText
-from mezzanine.utils.models import AdminThumbMixin
+from mezzanine.utils.models import AdminThumbMixin, upload_to
+from django.utils.translation import ugettext_lazy as _
 
 
 class Content(Displayable, RichText, AdminThumbMixin):
-    
-    pass
+    featured_image = FileField(verbose_name=_("Featured Image"),
+                               upload_to=upload_to("owacontent.Content.featured_image", "content"),
+                               format="Image", max_length=255, null=True, blank=True)
+    content_type = models.CharField(max_length=100, null=False, default="article")
+    featured_piece = models.BooleanField(default=False)
+    featured_piece_slide_image = FileField(verbose_name=_("Featured Piece Image"),
+                               upload_to=upload_to("owacontent.Content.featured_piece_slide_image", "content"),
+                               format="Image", max_length=255, null=True, blank=True)
+    admin_thumb_field = "featured_image"
+
+    class Meta:
+        verbose_name = _("Content")
+        verbose_name_plural = _("Content")
+        ordering = ("-publish_date",)
+
+    def get_absolute_url(self):
+        return reverse("content_detail", kwargs={"slug": self.slug})
--- owacontent/views.py
+++ owacontent/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from owacontent.models import Content
+
+
 def homepage(request, template):
     context = dict()
+    featured_content = Content.objects.all().filter(featured_piece=True)[:5]
+    if featured_content is not None and len(featured_content) > 0:
+        context["featured_content"] = featured_content
+        print "Featured content: ", featured_content
+    content_list = Content.objects.all()[:5]
+    context["home_content_list"] = content_list
+
     return render(request, template, context=context)
 
 
